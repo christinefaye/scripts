acc90da527b6aebf71eb3d2bf1b4a3cf89b6570b
christinefaye <christinevaldez@hotmail.com>
2015-11-20 13:59:33 -0500

    Updated bandit.py

--- tools/commonTools/overTheWire/bandit.py
+++ tools/commonTools/overTheWire/bandit.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python
 
+import sys
+sys.path.append("/home/christine/st/scripts/tools/commonMod")
+from cmHost import cHost
+
+def main():
+  url='bandit.labs.overthewire.org'
+  usr='bandit0'
+  pw='bandit0'
+
+  # Bandit host
+  b = cHost(usr,pw,url=url)
+
+if __name__=="__main__":
+  main()
 
