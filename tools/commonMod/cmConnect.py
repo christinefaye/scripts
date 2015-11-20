b3113efa3f772a66c61fdf47921c1156f7cf461d
christinefaye <christinevaldez@hotmail.com>
2015-11-20 13:54:26 -0500

    Adding cmConnect.py

---
+++ tools/commonMod/cmConnect.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+#TODO:
+
+'''
+Module to connect to a host
+
+'''
+
+class cConnect:
+  ''' Connection to host class '''
+  def __init__(self,user,passwd,ip=None,url=None):
+    self.usr = user		# Host username
+    self.pwd = passwd		# Host password
+    self.ip = ip		# Host ip address
+    self.url = url		# Host url
+#----------END CLASS-------------#
+
+
+
+
+
